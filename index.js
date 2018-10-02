a952fc852566a56ae458e02f1fc784651e51f07b
Cristian Ioanin <cristian.ioanin@gmail.com>
2018-10-03 00:51:48 +0300

    add side-menu functionality on phone screens < 600px width
    add javascript code to handle side-menu functionality;
    add simple hamburger menu icon using svg drawing

---
+++ index.js
@@ -0,0 +1,24 @@
+class SlideMenu {
+  constructor() {
+    this.leftSideMenu = document.querySelector('#left-pane');
+    this.closeButton = document.querySelector('.side-menu__close');
+  }
+
+  open() {
+    this.leftSideMenu.style.width = '26rem';
+    setTimeout(() => {
+      this.closeButton.style.position = 'fixed';
+    }, 400);
+  }
+
+  close() {
+    this.leftSideMenu.style.width = '0';
+    this.closeButton.style.position = 'absolute';
+  }
+}
+
+const initializeSlideMenu = function () {
+  window.slideMenu = new SlideMenu();
+}
+
+document.addEventListener('DOMContentLoaded', initializeSlideMenu);
